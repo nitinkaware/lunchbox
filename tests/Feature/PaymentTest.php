c4d4fad5f76575011364c0c53bfddef9a143b7ea
nitinkaware <nitin.kaware@synergenie.com>
2018-04-17 19:35:29 +0530

    update payment test.

--- tests/Feature/PaymentTest.php
+++ tests/Feature/PaymentTest.php
@@ -22,12 +22,14 @@ class PaymentTest extends TestCase {
     /** @test */
     function an_order_id_is_required_for_the_payment()
     {
+        $this->actingAs(factory(User::class)->create());
         $this->postJson(route('payments.store'))->assertStatus(422)->assertJsonValidationErrors('order_id');
     }
 
     /** @test */
     function an_order_id_must_be_an_array_and_not_empty()
     {
+        $this->actingAs(factory(User::class)->create());
         $this->postJson(route('payments.store'), [
             'order_id' => 1,
         ])->assertStatus(422)->assertJsonValidationErrors('order_id');
@@ -40,6 +42,8 @@ class PaymentTest extends TestCase {
     /** @test */
     function an_order_id_array_must_present_in_database_and_that_order_should_be_for_logged_in_user()
     {
+        $this->actingAs(factory(User::class)->create());
+
         $this->postJson(route('payments.store'), [
             'order_id' => [1],
         ])->assertStatus(422)->assertJsonValidationErrors('order_id.0');
@@ -54,8 +58,6 @@ class PaymentTest extends TestCase {
     /** @test */
     function a_user_can_make_payment_for_a_order()
     {
-        $this->withoutExceptionHandling();
-
         /** @var User $john */
         $john = factory(User::class)->create();
         $jeffery = factory(User::class)->create();
